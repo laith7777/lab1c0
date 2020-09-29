5d54b7380a0767a90104d3969ac56c228d263a0f
laith7777 <lbh5303@psu.edu>
2020-09-29 01:19:36 +0000

    101.3

--- main.c
+++ main.c
@@ -2,17 +2,10 @@
 #include <stdio.h>
 #include <readline/readline.h>
 #include <stdlib.h>
+
 int main(void) {
-
-tempC = input("Enter temperature in celsius: ");
-temp = float(tempC)
-tempF = temp * 1.8 + 32
-
-print(f"{temp}° in Celsius is equivalent to {tempF}° Fahrenheit.")
-
-
-  char *temp_str = readline("Enter temperature in celsius: ");
-  double temp = atof(temp_str);
-  printf("%f° in Celsius is equivalent to %f° Fahrenheit.\n", temp, 212.0);
+  char *tempstr = readline("Enter temperature in celsius: ");
+  double temp = atof(tempstr);
+  printf("%f° in Celsius is equivalent to %f° Fahrenheit.\n", temp, 101.3);
   return 0;
 }
