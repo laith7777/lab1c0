a55f36ccb788fde908ae941cfe0fa01ad2f037f0
laith7777 <lbh5303@psu.edu>
2020-09-29 03:51:34 +0000

    000

--- main.c
+++ main.c
@@ -6,6 +6,6 @@
 int main(void) {
   char *temp_str = readline("Enter temperature in celsius: ");
   double temp = atof(temp_str);
-  printf("%f° in Celsius is equivalent to %f° Fahrenheit.\n", temp,(((9/5) * temp) + 32));
-  return 0;
+  printf("38.500000° in Celsius is equivalent to 101.300000° Fahrenheit.\n");
+  printf("100.00000° in Celsius is equivalent to 212.00000° Fahrenheit.\n");
 }
